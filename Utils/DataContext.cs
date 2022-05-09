79cd139e7eacfc917fca29bb9d1a1ddff9852421
Luis J. Montes <luisjavm3@gmail.com>
2022-05-09 15:14:52 -0500

    Setup DbContext

---
+++ Utils/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using rtoken1.Model;
+
+namespace rtoken1.Utils
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
